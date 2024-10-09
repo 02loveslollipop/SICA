6b9c48d0e1fd10df22967f28fd454c2069611eee
02loveslollipop <mmonsalve2005@gmail.com>
2024-10-08 23:19:44 -0500

    Added _isActive atribute to Provider

--- provider.py
+++ provider.py
@@ -4,11 +4,11 @@ class Provider:
     def bulk_from_dict(data: dict) -> list['Provider']:
         return [Provider(**item) for item in data]
     
-    def __init__(self,name: str, address: str, _id: int = None) -> None:
+    def __init__(self,name: str, address: str, _id: int = None, _isActive: bool = True) -> None:
         self.name = name
         self.address = address
         self._id = _id
-        self._isActive = True
+        self._isActive = _isActive
         #TODO: Add more fields as needed
 
     def to_dict(self) -> dict:
@@ -19,4 +19,4 @@ class Provider:
         } #TODO: Add more fields as needed
     
     def __str__(self) -> str:
-        return self.name
+        return self.name + ' ' + self.address + ' ' + str(self._id) + ' ' + f"{'Active' if self._isActive else 'Inactive'}"
