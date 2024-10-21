673ca1187769972a181228ae53e0a033852d7a77
Emanuel Rios <118300687+EmanuelR521@users.noreply.github.com>
2024-10-21 09:48:30 -0500

    Added _isActive to Product

--- product.py
+++ product.py
@@ -4,12 +4,14 @@ class Product:
     def bulk_from_dict(data: dict) -> list['Product']:
         return [Product(**item) for item in data]
 
-    def __init__(self, name: str,description: str,category: str,price: float,status: str) -> None:  
+    def __init__(self, name: str,description: str,category: str,price: float,status: str,_id: int = None,_isActive: bool = True) -> None:  
         self.name = name
         self.description = description
         self.category = category
         self.price = price
         self.status = status
+        self._id = _id
+        self._isActive = _isActive
 
 
     def to_dict(self) -> dict:
@@ -19,6 +21,6 @@ class Product:
         'category': self.category,
         'price': self.price,
         'status': self.status,
-        'precio': self.precio
+        '_isActive': self._isActive	
         } 
     
