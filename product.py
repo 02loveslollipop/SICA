5e3fd1d7542fba44a3f2eefc447291269d5ff305
Emanuel Rios <118300687+EmanuelR521@users.noreply.github.com>
2024-10-22 08:16:59 -0500

    Added attribute quantity to product

--- product.py
+++ product.py
@@ -4,14 +4,16 @@ class Product:
     def bulk_from_dict(data: dict) -> list['Product']:
         return [Product(**item) for item in data]
 
-    def __init__(self, name: str,description: str,category: str,price: float,status: str,_id: int = None,_isActive: bool = True) -> None:  
+    def __init__(self, name: str,description: str,category: str,price: float,status: str,quantity: int,_id: int = None,_isActive: bool = True) -> None:  
+        self._id = _id
         self.name = name
         self.description = description
         self.category = category
         self.price = price
         self.status = status
-        self._id = _id
+        self.quantity = quantity
         self._isActive = _isActive
+
 
 
     def to_dict(self) -> dict:
@@ -21,6 +23,7 @@ class Product:
         'category': self.category,
         'price': self.price,
         'status': self.status,
+        'quantity' : self.quantity,
         '_isActive': self._isActive	
         } 
     
