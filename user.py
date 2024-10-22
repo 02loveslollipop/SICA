f94b873ae0f5c807828c661f4491fbf379b9feb7
Emanuel Rios <118300687+EmanuelR521@users.noreply.github.com>
2024-10-22 09:02:46 -0500

    Added missing attributes to user

--- user.py
+++ user.py
@@ -4,8 +4,11 @@ class User :
     def bulk_from_dict(data: dict) -> list['User']:
         return [User(**item) for item in data]
 
-    def __init__(self, username: str,password: str,role: str,_id: int = None,_isActive: bool = True) -> None:  
-        self.username = username
+    def __init__(self, name: str,lastname: str,email: str,cellphone: int,password: str,role: str,_id: int = None,_isActive: bool = True) -> None:  
+        self.name = name
+        self.lastname = lastname
+        self.email = email
+        self.cellphone = cellphone
         self.password = password
         self.role = role
         self._id = _id
@@ -13,7 +16,10 @@ class User :
 
     def to_dict(self) -> dict:
         return {
-        'username': self.username,
+        'name': self.name,
+        'lastname': self.lastname,
+        'email': self.email,
+        'cellphone': self.cellphone,
         'password': self.password,
         'role': self.role,
         '_isActive': self.isActive
