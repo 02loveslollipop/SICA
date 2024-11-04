f62630353780f26814851b54fb4098c1ee4b85db
02loveslollipop <mmonsalve2005@gmail.com>
2024-11-04 17:22:47 -0500

    feat: Add API token configuration to Secret class

--- secret.py
+++ secret.py
@@ -9,3 +9,5 @@ class Secret:
             self.uri = config['MongoDB']['uri']
             self.dbName = config['MongoDB']['dbName']
             self.providerCollection = config['MongoDB']['providerCollection']
+            self.token_ttl = config['API']['token_ttl']
+            self.secret = config['API']['secret']
